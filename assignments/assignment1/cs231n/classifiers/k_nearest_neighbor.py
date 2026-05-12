285baca810ab037d34b92857cfe57cd419f86bf4
Muelsyseqwq <1615844471@qq.com>
2026-05-12 19:26:01 +0800

    update

--- assignments/assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignments/assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -95,7 +95,7 @@ class KNearestNeighbor(object):
             # points, and store the result in dists[i, :].                        #
             # Do not use np.linalg.norm().                                        #
             #######################################################################
-            dists[i:] = np.sqrt(np.sum((X[i] - self.X_train) ** 2,axis=1))
+            dists[i, :] = np.sqrt(np.sum((X[i] - self.X_train) ** 2,axis=1))
         return dists
 
     def compute_distances_no_loops(self, X):
